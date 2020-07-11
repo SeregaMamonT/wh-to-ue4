ba05050588dbf9d5fec50e88d06a4df65ad35d0a
SeregaMamonT <mamont@informatics.by>
2020-07-11 19:05:46 +0300

    Add empty list readers for structures before PARTICLE_EMITTER_LIST

--- src/wh_parser.py
+++ src/wh_parser.py
@@ -13,8 +13,23 @@ def parse_file(file: BinaryIO):
     assert_version('Root serializer', 23, int2(file))
 
     buildings = read_building_list(file)
-    file.read(74)
+    read_building_list_far(file)
+    read_capture_location_set(file)
+    read_ef_line_list(file)
+    read_go_outlines(file)
+    read_non_terrain_outlines(file)
+    read_zones_template_list(file)
+    read_prefab_instance_list(file)
+    read_bmd_outline_list(file)
+    read_terrain_outlines(file)
+    read_lite_building_outlines(file)
+    read_camera_zones(file)
+    read_civilian_deployment_list(file)
+    read_civilian_shelter_list(file)
+    read_prop_list(file)
     particles = read_particle_list(file)
+
+    # rest of file
 
     return buildings
 
@@ -55,9 +70,81 @@ def read_building_instance(file: BinaryIO):
     return instance
 
 
+def read_building_list_far(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "BATTLEFIELD_BUILDING_LIST_FAR has items"
+
+
+def read_capture_location_set(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "CAPTURE_LOCATION_SET has items"
+
+
+def read_ef_line_list(file: BinaryIO):
+    assert int4(file) == 0, "EF_LINE_LIST has items"
+
+
+def read_go_outlines(file: BinaryIO):
+    assert int4(file) == 0, "GO_OUTLINES has items"
+
+
+def read_non_terrain_outlines(file: BinaryIO):
+    assert int4(file) == 0, "NON_TERRAIN_OUTLINES has items"
+
+
+def read_zones_template_list(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "ZONES_TEMPLATE_LIST has items"
+
+
+def read_prefab_instance_list(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "PREFAB_INSTANCE_LIST has items"
+
+
+def read_bmd_outline_list(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "BMD_OUTLINE_LIST has items"
+
+
 def read_particle_list(file: BinaryIO):
     assert_version('PARTICLE_EMITTER_LIST', 1, int2(file))
     return read_list(file, read_particle_instance)
+
+
+def read_terrain_outlines(file: BinaryIO):
+    assert int4(file) == 0, "TERRAIN_OUTLINES has items"
+
+
+def read_lite_building_outlines(file: BinaryIO):
+    assert int4(file) == 0, "LITE_BUILDING_OUTLINES has items"
+
+
+def read_camera_zones(file: BinaryIO):
+    int2(file) # version
+    assert int4(file) == 0, "CAMERA_ZONES has items"
+
+
+def read_civilian_deployment_list(file: BinaryIO):
+    assert int4(file) == 0, "CIVILIAN_DEPLOYMENT_LIST has items"
+
+
+def read_civilian_shelter_list(file: BinaryIO):
+    assert int4(file) == 0, "CIVILIAN_SHELTER_LIST has items"
+
+
+def read_prop_list(file: BinaryIO):
+    assert_version('PROP_LIST', 2, int2(file))
+    keys = read_list(file, read_prop_key_instance)
+    props = read_list(file, read_prop_prop_instance)
+
+
+def read_prop_key_instance(file: BinaryIO):
+    pass
+
+
+def read_prop_prop_instance(file: BinaryIO):
+    pass
 
 
 def read_particle_instance(file: BinaryIO):
