fd2e6bc534c867971c4611ea8cc2130aca076c4c
Angmarec <collectionspam@mail.ru>
2020-07-14 20:46:30 +0300

    added version_reader for spot light and point light parsers

--- src/parsers/point_light_list.py
+++ src/parsers/point_light_list.py
@@ -4,30 +4,56 @@ from reader import bool1, string, int1, int2, int4, float4, read_list, assert_ve
     read_translation, get_scale, unscale
 
 
+def read_point_light_v5(file):
+    position = (float4(file), float4(file), float4(file))
+    radius = float4(file)
+    colour = (float4(file), float4(file), float4(file))
+    colour_scale = float4(file)
+    animation_type = int1(file)
+    params = (float4(file), float4(file))
+    # animation_type = float4(file)
+    colour_min = float4(file)
+    random_offset = float4(file)
+    falloff_type = string(file)
+    lf_relative = bool1(file)
+    height_mode = string(file)
+    light_probes_only = bool1(file)
+    pdlc_mask = int4(file)
+
+def read_point_light_v6(file):
+    position = (float4(file), float4(file), float4(file))
+    radius = float4(file)
+    colour = (float4(file), float4(file), float4(file))
+    colour_scale = float4(file)
+    animation_type = int1(file)
+    params = (float4(file), float4(file))
+    # animation_type = float4(file)
+    colour_min = float4(file)
+    random_offset = float4(file)
+    falloff_type = string(file)
+    lf_relative = bool1(file)
+    height_mode = string(file)
+    light_probes_only = bool1(file)
+    pdlc_mask = int8(file)
+
+
+version_readers = {
+    5: read_point_light_v5,
+    6: read_point_light_v6,
+}
+
+
+def get_version_reader(version):
+    if version in version_readers:
+        return version_readers[version]
+    else:
+        raise Exception('Unsupported point light version: ' + str(version))
+
+
 def read_point_light_list(file: BinaryIO):
     version = int2(file)  # version
     point_lights = int4(file)
     # print("Point lights: ", version, point_lights)
-    # print('Current file position: ' + hex(input_file.tell()).upper())
     for i in range(point_lights):
-        point_ligh_version = int2(file)
-        position = (float4(file), float4(file), float4(file))
-        radius = float4(file)
-        colour = (float4(file), float4(file), float4(file))
-        colour_scale = float4(file)
-        # file.read(9)
-        animation_type = int1(file)
-        params = (float4(file), float4(file))
-        # animation_type = float4(file)
-        colour_min = float4(file)
-        random_offset = float4(file)
-        falloff_type = string(file)
-        lf_relative = bool1(file)
-        height_mode = string(file)
-        light_probes_only = bool1(file)
-        if point_ligh_version == 5:
-            pdlc_mask = int4(file)
-        elif point_ligh_version == 6:
-            pdlc_mask = int8(file)
-        # print(point_ligh_version, position, radius, colour, colour_scale, animation_type, params, colour_min, random_offset, falloff_type, height_mode, pdlc_mask)
-    # assert int4(file) == 0, "POINT_LIGHT_LIST has items"
+        point_light_version = int2(file)
+        get_version_reader(point_light_version)(file)
--- src/parsers/spot_light_list.py
+++ src/parsers/spot_light_list.py
@@ -4,29 +4,54 @@ from reader import bool1, string, int1, int2, int4, float4, read_list, assert_ve
     read_translation, get_scale, unscale
 
 
+def read_spot_light_v5(file):
+    position = (float4(file), float4(file), float4(file))
+    end = (float4(file), float4(file), float4(file), float4(file))
+    length = float4(file)
+    inner_angle = float4(file)
+    outer_angle = float4(file)
+    colour = (float4(file), float4(file), float4(file))
+    falloff = float4(file)
+    gobo = string(file)
+    volumetric = bool1(file)
+    height_mode = string(file)
+    pdlc_mask = int4(file)
+    # print(position, gobo, pdlc_mask)
+
+
+def read_spot_light_v6(file):
+    position = (float4(file), float4(file), float4(file))
+    end = (float4(file), float4(file), float4(file), float4(file))
+    length = float4(file)
+    inner_angle = float4(file)
+    outer_angle = float4(file)
+    colour = (float4(file), float4(file), float4(file))
+    falloff = float4(file)
+    gobo = string(file)
+    volumetric = bool1(file)
+    height_mode = string(file)
+    pdlc_mask = int8(file)
+    # print(position, gobo, pdlc_mask)
+
+
+
+version_readers = {
+    5: read_spot_light_v5,
+    6: read_spot_light_v6,
+}
+
+
+def get_version_reader(version):
+    if version in version_readers:
+        return version_readers[version]
+    else:
+        raise Exception('Unsupported spot light version: ' + str(version))
+
+
 def read_spot_light_list(file: BinaryIO):
     version = int2(file)  # version
     spot_lights = int4(file)
     # print("Spot lights: ", version, spot_lights)
     for i in range(spot_lights):
         spot_light_version = int2(file)
-        position = (float4(file), float4(file), float4(file))
-        end = (float4(file), float4(file), float4(file), float4(file))
-        length = float4(file)
-        inner_angle = float4(file)
-        outer_angle = float4(file)
-        colour = (float4(file), float4(file), float4(file))
-        falloff = float4(file)
-
-        gobo = string(file)
-        volumetric = bool1(file)
-
-        height_mode = string(file)
-        if spot_light_version == 5:
-            pdlc_mask = int4(file)
-        elif spot_light_version == 6:
-            pdlc_mask = int8(file)
-        # print(spot_light_version, position, end, length, inner_angle, outer_angle, colour, falloff, height_mode,
-        #       pdlc_mask, gobo, volumetric)
-
-    # assert int4(file) == 0, "SPOT_LIGHT_LIST has items"
+        get_version_reader(spot_light_version)(file)
