55a22247d48cdea699fd154e62ee763e386e2547
Angmarec <collectionspam@mail.ru>
2020-07-15 21:04:30 +0300

    added class containing all prefab info

--- src/parsers/light_probe_list.py
+++ src/parsers/light_probe_list.py
@@ -3,16 +3,26 @@ from typing import BinaryIO
 from reader import bool1, string, int1, int2, int4, float4, read_list, assert_version, int8, read_coordinates, \
     read_translation, get_scale, unscale
 
+from wh_binary_objects import LightProbe
+
+
+
+def read_light_probe(file):
+    light_probe = LightProbe()
+    light_probe.position = (float4(file), float4(file), float4(file))
+    light_probe.radius = float4(file)
+    light_probe.is_primary = bool1(file)
+    light_probe.height_mode = string(file)
+
+    return light_probe
+
 
 def read_light_probe_list(file: BinaryIO):
     version = int2(file)  # version
     amount = int4(file)
-    # print('Lightprobe: ', version, amount)
+    light_probe_list = []
     for i in range(amount):
-        light_probe_version = int2(file)
-        t = (float4(file), float4(file), float4(file))
-        radius = float4(file)
-        is_primary = bool1(file)
-        height_mode = string(file)
-        # print(t, radius, is_primary, height_mode)
-    # assert int4(file) == 0, "LIGHT_PROBE_LIST has items"
+        point_light_version = int2(file)
+        light_probe_list.append(read_light_probe(file))
+
+    return light_probe_list
--- src/parsers/terrain_stencil_triangle_list.py
+++ src/parsers/terrain_stencil_triangle_list.py
@@ -3,16 +3,25 @@ from typing import BinaryIO
 from reader import bool1, string, int1, int2, int4, float4, read_list, assert_version, int8, read_coordinates, \
     read_translation, get_scale, unscale
 
+from wh_binary_objects import TerrainStencilTriangle
+
+
+def read_terrain_stencil_triangle(file):
+    terrain_stencil_triangle = TerrainStencilTriangle()
+    terrain_stencil_triangle.position1 = (float4(file), float4(file), float4(file))
+    terrain_stencil_triangle.position2 = (float4(file), float4(file), float4(file))
+    terrain_stencil_triangle.position3 = (float4(file), float4(file), float4(file))
+    terrain_stencil_triangle.height_mode = string(file)
+
+    return terrain_stencil_triangle
+
 
 def read_terrain_stencil_triangle_list(file: BinaryIO):
     version = int2(file)  # version
-    terrain_stencil_triangles = int4(file)
-    for i in range(terrain_stencil_triangles):
+    amount = int4(file)
+    terrain_stencil_triangle_list = []
+    for i in range(amount):
         terrain_stencil_triangle_version = int2(file)
-        position0 = (float4(file), float4(file), float4(file))
-        position1 = (float4(file), float4(file), float4(file))
-        position2 = (float4(file), float4(file), float4(file))
-        height_mode = string(file)
-        # print(terrain_stencil_triangle_version, position0, position1, position2, height_mode)
+        terrain_stencil_triangle_list.append(read_terrain_stencil_triangle(file))
 
-    # assert int4(file) == 0, "TERRAIN_STENCIL_TRIANGLE_LIST has items"
+    return terrain_stencil_triangle_list
