a0aec8cad605044a896c5ae619df8afab99e972d
Angmarec <collectionspam@mail.ru>
2020-07-17 13:28:37 +0300

    saver work continues

--- src/terry_savers/terry_buildings_list.py
+++ src/terry_savers/terry_buildings_list.py
@@ -5,34 +5,27 @@ from os import listdir, path
 from xml.etree.ElementTree import Element, SubElement, tostring
 from typing import Dict, List
 from wh_binary_to_terry_convertor import convert_building
+from terry_savers.xml_saver_utils import ectransform_to_xml, ecmeshrendersettings_to_xml
+from terry_savers.xml_saver_utils import format_float
 
+from wh_binary_objects import Building
 from wh_terry_objects import TerryBuilding
 
-def format_float(x):
-    return "{:.5f}".format(x).rstrip("0").strip(".")
-
-def save_buildings_list(buildings: List[TerryBuilding], entities: Element):
+def save_buildings_list(buildings: List[Building], entities: Element):
     for building in buildings:
         terry_building = convert_building(building)
         entity = SubElement(entities, "entity", {"id": hex(random.randrange(10 ** 17, 10 ** 18))[2:]})
         ECBuilding = SubElement(entity, "ECBuilding", {
-            "key": 'terry_building.key',
-            "damage": map(format_float, terry_building.damage),
+            "key": terry_building.key,
+            "damage": str(terry_building.damage),
             "indestructible": "false",
             "toggleable": "false",
             "capture_location": "",
             "export_as_prop": "false",
             "allow_in_outfield_as_prop": "false"
         })
-        ECMeshRenderSettings = SubElement(entity, "ECMeshRenderSettings", {
-            "cast_shadow": "true"
-        })
-
-        # ECTransform = SubElement(entity, "ECTransform", {
-        #     "position": " ".join(map(format_float, model["position"])),
-        #     "rotation": " ".join(map(format_float, model["rotation"])),
-        #     "scale": " ".join(map(format_float, model["scale"])),
-        # })
+        ecmeshrendersettings_to_xml(entity, terry_building.ecmeshrendersettings)
+        ectransform_to_xml(entity, terry_building.ectransform)
 
         ECTerrainClamp = SubElement(entity, "ECTerrainClamp", {
             "active": "false",
