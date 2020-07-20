49d9636a0bbdbf0b9f219f54d8fa79e88ce61c7f
Angmarec <collectionspam@mail.ru>
2020-07-20 21:07:30 +0300

    cleaned up props

--- src/parsers/prop_list.py
+++ src/parsers/prop_list.py
@@ -5,11 +5,13 @@ from reader import bool1, string, int1, int2, int4, float4, read_list, assert_ve
 
 from wh_binary_objects import Prop
 
+from version_holder import VersionHolder
+
 
 def read_prop_list(file: BinaryIO):
     assert_version('PROP_LIST', 2, int2(file))
     keys = read_list(file, read_prop_key_instance)
-    props = read_list(file, read_prop_prop_instance)
+    props = read_list(file, read_prop_instance)
     result = {}
     for key in keys:
         result[key] = []
@@ -17,6 +19,7 @@ def read_prop_list(file: BinaryIO):
         key = keys[prop.key_index]
         prop.key = key
         result[key].append(prop)
+
     return result
 
 
@@ -24,46 +27,53 @@ def read_prop_key_instance(file: BinaryIO):
     return string(file)
 
 
-def read_prop_prop_instance(file: BinaryIO):
-    # assert_version('PROP', 15, int2(file))
+def read_prop_instance(file):
+    prop_instance_version = int2(file)
+    return prop_instance_versions.get_reader(prop_instance_version)(file)
 
-    version = int2(file)
-    # print('Prop version: ', version)
 
+def read_prop_instance_v14(file):
+    prop = read_prop_instance_common(file)
+    strange_number = int4(file)
+    bool1(file)
+    bool1(file)
+
+    return prop
+
+
+def read_prop_instance_v15(file):
+    prop = read_prop_instance_common(file)
+    strange_number = int8(file)
+    bool1(file)
+    bool1(file)
+
+    return prop
+
+
+def read_prop_instance_common(file):
     prop = Prop()
     prop.key_index = int2(file)
     file.read(2)  # TODO
-
     prop.transform = read_transform_4_x_3(file)
     prop.decal = bool1(file)
-    # print('Decal: ', prop.decal)
     file.read(7)  # flags from logic_decal to animated
     prop.decal_parallax_scale = float4(file)
     # print('Decal parallax: ', prop.decal_parallax_scale)
     prop.decal_tiling = float4(file)
     # print('Decal tiling: ', prop.decal_tiling)
     decal_override_gbuffer_normal = bool1(file)
-
     prop.flags = read_flags(file)
     prop.flags["decal_override_gbuffer_normal"] = decal_override_gbuffer_normal
-
     prop.flags["visible_in_shroud"] = bool1(file)
     prop.flags["decal_apply_to_terrain"] = bool1(file)
     prop.flags["decal_apply_to_gbuffer_objects"] = bool1(file)
     prop.flags["decal_render_above_snow"] = bool1(file)
-
     prop.height_mode = string(file)
-    # print('Height mode: ', prop.height_mode)
-    if version == 15:
-        strange_number = int8(file)
-        # print('Strange number: ', strange_number)
-        bool1(file)
-        bool1(file)
-    elif version == 14:
-        strange_number = int4(file)
-        # print('Strange number: ', strange_number)
-        bool1(file)
-        bool1(file)
-    # print(int8(file))
 
     return prop
+
+
+prop_instance_versions = VersionHolder('Prop', {
+    14: read_prop_instance_v14,
+    15: read_prop_instance_v15,
+})
